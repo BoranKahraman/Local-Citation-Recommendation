9c198becd6b24f0dcca3b09cbde072d02c3af22d
Boran Kahraman <93979561+BoranKahraman@users.noreply.github.com>
2023-10-22 21:01:17 +0300

    testlerin yapılacağı py dosyası

---
+++ src/test.py
@@ -0,0 +1,8 @@
+from main import *
+
+
+pdf2textconverter = Pdf2Text()
+
+example = pdf2textconverter.pdf2txt('__retim __li__kileri Mi,__retim G____leri Mi_ Te__ekk__rler.Almayay__m![#582]-502.pdf')
+
+print(example)
